684d261cdee52503f05847e2f1958be6411da9a6
Vlad <vladjdk@gmail.com>
2025-07-03 13:46:30 -0400

    test disallow transferring vesting ERC20

--- precompiles/erc20/tx_test.go
+++ precompiles/erc20/tx_test.go
@@ -1,6 +1,10 @@
 package erc20_test
 
 import (
+	"cosmossdk.io/math"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+	"github.com/holiman/uint256"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/core/vm"
@@ -64,6 +68,47 @@ func (s *PrecompileTestSuite) TestTransfer() {
 			"fail - not enough balance",
 			func() []interface{} {
 				return []interface{}{toAddr, big.NewInt(2e18)}
+			},
+			func() {},
+			true,
+			erc20.ErrTransferAmountExceedsBalance.Error(),
+		},
+		{
+			"fail - not enough balance, sent amount is being vested",
+			func() []interface{} {
+				ctx := s.network.GetContext()
+				accAddr := sdk.AccAddress(fromAddr.Bytes())
+				err := s.network.App.BankKeeper.SendCoins(ctx, s.keyring.GetAccAddr(0), accAddr, sdk.NewCoins(sdk.NewCoin(s.network.GetBaseDenom(), math.NewInt(2e18))))
+				s.Require().NoError(err)
+				// replace with vesting account
+				balanceResp, err := s.grpcHandler.GetBalanceFromEVM(accAddr)
+				s.Require().NoError(err)
+
+				balance, ok := math.NewIntFromString(balanceResp.Balance)
+				s.Require().True(ok)
+
+				baseAccount := s.network.App.AccountKeeper.GetAccount(ctx, accAddr).(*authtypes.BaseAccount)
+				baseDenom := s.network.GetBaseDenom()
+				currTime := s.network.GetContext().BlockTime().Unix()
+				acc, err := vestingtypes.NewContinuousVestingAccount(baseAccount, sdk.NewCoins(sdk.NewCoin(baseDenom, balance)), s.network.GetContext().BlockTime().Unix(), currTime+100)
+				s.Require().NoError(err)
+				s.network.App.AccountKeeper.SetAccount(ctx, acc)
+
+				spendable := s.network.App.BankKeeper.SpendableCoin(ctx, accAddr, baseDenom).Amount
+				s.Require().Equal(spendable.String(), "0")
+
+				evmBalanceRes, err := s.grpcHandler.GetBalanceFromEVM(accAddr)
+				s.Require().NoError(err)
+				evmBalance := evmBalanceRes.Balance
+				s.Require().Equal(evmBalance, "0")
+
+				tb, overflow := uint256.FromBig(s.network.App.BankKeeper.GetBalance(ctx, accAddr, baseDenom).Amount.BigInt())
+				s.Require().False(overflow)
+				s.Require().Equal(tb.ToBig(), balance.BigInt())
+
+				return []interface{}{
+					toAddr, big.NewInt(2e18),
+				}
 			},
 			func() {},
 			true,
