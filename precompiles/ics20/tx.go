e29ad3c7c07e8d909b7706965d0865a2b852d926
Eric Warehime <eric.warehime@gmail.com>
2025-07-10 15:58:07 -0700

    Squashed commit of the following:
    commit b33fae6210000b23693e20bc4ecf0abb2b2b490e
    Author: aljo242 <alex@interchainlabs.io>
    Date:   Thu Jul 10 11:44:51 2025 -0400
    
        fix
    
    commit dbe340066b1159165b762475cecede607b6ab7b9
    Merge: 330dda7 79d4d5b
    Author: Alex | Interchain Labs <alex@interchainlabs.io>
    Date:   Wed Jul 9 13:00:32 2025 -0400
    
        Merge branch 'main' into 513-transfer
    
    commit 330dda7c1b87fbcb0d5cb83876b4dfe6476085d4
    Merge: 7f04eff ed7916c
    Author: Alex | Interchain Labs <alex@interchainlabs.io>
    Date:   Tue Jul 1 11:53:24 2025 -0400
    
        Merge branch 'main' into 513-transfer
    
    commit 7f04eff73a17a2c60ea69107455ab3de9db716df
    Author: aljo242 <alex@interchainlabs.io>
    Date:   Mon Jun 30 09:56:16 2025 -0400
    
        todo
    
    commit 5c9f1965d00a2a34886101783b0bbd831f82abdb
    Author: aljo242 <alex@interchainlabs.io>
    Date:   Mon Jun 30 09:55:27 2025 -0400
    
        validation
    
    commit 0f816390f7cb4b11c83200290e06e1f4d1b174f0
    Author: aljo242 <alex@interchainlabs.io>
    Date:   Mon Jun 30 09:51:00 2025 -0400
    
        inital-sketch

--- precompiles/ics20/tx.go
+++ precompiles/ics20/tx.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 
 	cmn "github.com/cosmos/evm/precompiles/common"
+	transfertypes "github.com/cosmos/ibc-go/v10/modules/apps/transfer/types"
+	connectiontypes "github.com/cosmos/ibc-go/v10/modules/core/03-connection/types"
 	channeltypes "github.com/cosmos/ibc-go/v10/modules/core/04-channel/types"
 	host "github.com/cosmos/ibc-go/v10/modules/core/24-host"
 
@@ -15,11 +17,75 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// TODO TEST suite for precompile
+
 const (
 	// TransferMethod defines the ABI method name for the ICS20 Transfer
 	// transaction.
 	TransferMethod = "transfer"
 )
+
+// validateV1TransferChannel does the following validation on an ibc v1 channel specified in a MsgTransfer:
+// - check if the channel exists
+// - check if the channel is OPEN
+// - check if the underlying connection exists
+// - check if the underlying connection is OPEN
+func (p *Precompile) validateV1TransferChannel(ctx sdk.Context, msg *transfertypes.MsgTransfer) error {
+	if msg == nil {
+		return fmt.Errorf("msg cannot be nil")
+	}
+
+	if err := msg.ValidateBasic(); err != nil {
+		return fmt.Errorf("msg invalid: %w", err)
+	}
+
+	// check if channel exists and is open
+	channel, found := p.channelKeeper.GetChannel(ctx, msg.SourcePort, msg.SourceChannel)
+	if !found {
+		return errorsmod.Wrapf(
+			channeltypes.ErrChannelNotFound,
+			"port ID (%s) channel ID (%s)",
+			msg.SourcePort,
+			msg.SourceChannel,
+		)
+	}
+	if err := channel.ValidateBasic(); err != nil {
+		return fmt.Errorf("channel invalid: %w", err)
+	}
+
+	// Validate channel is in OPEN state
+	if channel.State != channeltypes.OPEN {
+		return errorsmod.Wrapf(
+			channeltypes.ErrInvalidChannelState,
+			"channel (%s) is not open, current state: %s",
+			msg.SourceChannel,
+			channel.State.String(),
+		)
+	}
+
+	// Validate underlying connection exists and is active
+	connection, err := p.channelKeeper.GetConnection(ctx, channel.ConnectionHops[0])
+	if err != nil {
+		return errorsmod.Wrapf(
+			err,
+			"connection (%s) not found for channel (%s)",
+			channel.ConnectionHops[0],
+			msg.SourceChannel,
+		)
+	}
+
+	// Validate connection is in OPEN state
+	if connection.State != connectiontypes.OPEN {
+		return errorsmod.Wrapf(
+			connectiontypes.ErrInvalidConnectionState,
+			"connection (%s) is not open, current state: %s",
+			channel.ConnectionHops[0],
+			connection.State.String(),
+		)
+	}
+
+	return nil
+}
 
 // Transfer implements the ICS20 transfer transactions.
 func (p *Precompile) Transfer(
@@ -36,15 +102,8 @@ func (p *Precompile) Transfer(
 
 	// If the channel is in v1 format, check if channel exists and is open
 	if channeltypes.IsChannelIDFormat(msg.SourceChannel) {
-		// check if channel exists and is open
-		hasV1Channel := p.channelKeeper.HasChannel(ctx, msg.SourcePort, msg.SourceChannel)
-		if !hasV1Channel {
-			return nil, errorsmod.Wrapf(
-				channeltypes.ErrChannelNotFound,
-				"port ID (%s) channel ID (%s)",
-				msg.SourcePort,
-				msg.SourceChannel,
-			)
+		if err := p.validateV1TransferChannel(ctx, msg); err != nil {
+			return nil, err
 		}
 		// otherwise, it’s a v2 packet, so perform client ID validation
 	} else if v2ClientIDErr := host.ClientIdentifierValidator(msg.SourceChannel); v2ClientIDErr != nil {
