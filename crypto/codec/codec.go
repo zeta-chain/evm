2163003d3b6fcd687d00d00752a4d25e12552b5d
skosito <skostic9242@gmail.com>
2025-07-15 23:41:24 +0200

    try to use legacy types for needed ethermint proto

--- crypto/codec/codec.go
+++ crypto/codec/codec.go
@@ -2,6 +2,7 @@ package codec
 
 import (
 	"github.com/cosmos/evm/crypto/ethsecp256k1"
+	legacytypes "github.com/cosmos/evm/legacy/types"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
@@ -11,4 +12,8 @@ import (
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethsecp256k1.PubKey{})
 	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
+
+	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &legacytypes.PubKey{})
+	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &legacytypes.PrivKey{})
+
 }
